b0149ecc293726c134a7bb90f2b684b1b425188c
Xanhian <kyletasmoredjo2@gmail.com>
2024-06-15 02:51:46 -0300

    Update db_employer.sql

--- db_employer.sql
+++ db_employer.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 14, 2024 at 04:13 AM
+-- Generation Time: Jun 15, 2024 at 07:51 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.0.13
 
@@ -41,7 +41,10 @@ CREATE TABLE `contacts` (
 INSERT INTO `contacts` (`contact_id`, `email`, `address`, `phone_number`) VALUES
 (1, 'test', 'barbadosstraat#20', '123123'),
 (2, 'email@gmail.com', '2starr', '30'),
-(3, 'test', 'asd32', '2000');
+(3, 'test', 'asd32', '2000'),
+(4, 'sa@gma', 'asas', '213312'),
+(5, 'kyle@gmial.com', 'asdas', '#30'),
+(6, 'kyle@gmial.com', 'adp', '#30');
 
 -- --------------------------------------------------------
 
@@ -59,7 +62,10 @@ CREATE TABLE `skills` (
 --
 
 INSERT INTO `skills` (`skill_id`, `skill`) VALUES
-(1, 'testerbae');
+(1, 'testerbae'),
+(2, 'sassa'),
+(3, 'eten'),
+(4, 'lopen');
 
 -- --------------------------------------------------------
 
@@ -84,12 +90,10 @@ CREATE TABLE `students` (
 --
 
 INSERT INTO `students` (`student_id`, `name`, `last_name`, `student_number`, `age`, `birthyear`, `team_id`, `contact_id`, `skill_id`) VALUES
-(1, 'kylo', 'ren', 'tasd', NULL, NULL, NULL, NULL, NULL),
-(2, 'aap2wqw', NULL, 'as@sa', NULL, NULL, NULL, NULL, NULL),
 (3, 'Sam', 'Tasmio', 'A123', 12, '2002-02-01', NULL, 0, NULL),
-(4, 'Adam', 'palo', 'as1234', 12, '2002-02-02', 1, 1, 1),
 (5, 'God', 'tas', 'st123', 12, '2002-12-02', NULL, 2, NULL),
-(6, 'SAM', 'SAM', 'as12434', 12, '2002-12-01', NULL, 3, NULL);
+(7, 'kyle', 'sas', 'sasa', 12, '2002-12-02', NULL, 4, NULL),
+(9, 'Samuel', 'asdsa', 'test1234', 1334, '2002-10-12', 5, 2, 4);
 
 -- --------------------------------------------------------
 
@@ -107,7 +111,9 @@ CREATE TABLE `teams` (
 --
 
 INSERT INTO `teams` (`team_id`, `name`) VALUES
-(1, 'staarrss');
+(2, '4reteter'),
+(3, 'star'),
+(5, 'jokers');
 
 --
 -- Indexes for dumped tables
@@ -129,7 +135,9 @@ ALTER TABLE `skills`
 -- Indexes for table `students`
 --
 ALTER TABLE `students`
-  ADD PRIMARY KEY (`student_id`);
+  ADD PRIMARY KEY (`student_id`),
+  ADD KEY `team_key` (`team_id`),
+  ADD KEY `skill_key` (`skill_id`);
 
 --
 -- Indexes for table `teams`
@@ -145,25 +153,36 @@ ALTER TABLE `teams`
 -- AUTO_INCREMENT for table `contacts`
 --
 ALTER TABLE `contacts`
-  MODIFY `contact_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `contact_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `skills`
 --
 ALTER TABLE `skills`
-  MODIFY `skill_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `skill_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `students`
 --
 ALTER TABLE `students`
-  MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `teams`
 --
 ALTER TABLE `teams`
-  MODIFY `team_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `team_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `students`
+--
+ALTER TABLE `students`
+  ADD CONSTRAINT `skill_key` FOREIGN KEY (`skill_id`) REFERENCES `skills` (`skill_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `team_key` FOREIGN KEY (`team_id`) REFERENCES `teams` (`team_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
